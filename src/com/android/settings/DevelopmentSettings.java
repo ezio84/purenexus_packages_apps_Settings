9e39152c0fad0f616b2f8438b19353fc0ea892b9
ezio84 <brabus84@gmail.com>
2016-04-23 10:35:03 +0200

    Remove sRGB (color mode) settings from Dev settings, we have kcal support:
    with this entry, each time you open the Dev Settings, kcal settings are overwritten
    and you need to apply kcal settings again from your kernel customization app (like
    Kerneladiutor or the xda kcal app).
    
    Also, colors are washed out with the sRGB mode on Angler amoled; using kcal, and
    just setting the Saturation to 11 instead of 31 (the default aosp value) keeps vivid
    colors but fixes the over saturation (so no burned reds anymore :P ).

--- src/com/android/settings/DevelopmentSettings.java
+++ src/com/android/settings/DevelopmentSettings.java
@@ -118,7 +118,6 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
     private static final String BUGREPORT = "bugreport";
     private static final String OPENGL_TRACES_PROPERTY = "debug.egl.trace";
     private static final String TUNER_UI_KEY = "tuner_ui";
-    private static final String COLOR_TEMPERATURE_PROPERTY = "persist.sys.debug.color_temp";
 
     private static final String DEBUG_APP_KEY = "debug_app";
     private static final String WAIT_FOR_DEBUGGER_KEY = "wait_for_debugger";
@@ -158,8 +157,6 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
     private static final String WIFI_ALLOW_SCAN_WITH_TRAFFIC_KEY = "wifi_allow_scan_with_traffic";
     private static final String WIFI_LEGACY_DHCP_CLIENT_KEY = "legacy_dhcp_client";
     private static final String MOBILE_DATA_ALWAYS_ON = "mobile_data_always_on";
-    private static final String KEY_COLOR_MODE = "color_mode";
-    private static final String COLOR_TEMPERATURE_KEY = "color_temperature";
 
     private static final String INACTIVE_APPS_KEY = "inactive_apps";
 
@@ -260,10 +257,6 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
     private ListPreference mAppProcessLimit;
 
     private SwitchPreference mShowAllANRs;
-
-    private ColorModePreference mColorModePreference;
-
-    private SwitchPreference mColorTemperaturePreference;
 
     private Preference mChamber;
     private SwitchPreference mChamberUnlocked;
@@ -428,22 +421,6 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
             removePreferenceForProduction(hdcpChecking);
         }
 
-        mColorModePreference = (ColorModePreference) findPreference(KEY_COLOR_MODE);
-        mColorModePreference.updateCurrentAndSupported();
-        if (mColorModePreference.getTransformsCount() < 2) {
-            removePreference(KEY_COLOR_MODE);
-            mColorModePreference = null;
-        }
-
-        mColorTemperaturePreference = (SwitchPreference) findPreference(COLOR_TEMPERATURE_KEY);
-        if (getResources().getBoolean(R.bool.config_enableColorTemperature)) {
-            mAllPrefs.add(mColorTemperaturePreference);
-            mResetSwitchPrefs.add(mColorTemperaturePreference);
-        } else {
-            removePreference(COLOR_TEMPERATURE_KEY);
-            mColorTemperaturePreference = null;
-        }
-
         mChamber = (Preference) findPreference(KEY_CHAMBER_OF_SECRETS);
         mAllPrefs.add(mChamber);
         mChamberUnlocked =
@@ -580,18 +557,11 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
         }
         mSwitchBar.show();
 
-        if (mColorModePreference != null) {
-            mColorModePreference.startListening();
-            mColorModePreference.updateCurrentAndSupported();
-        }
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        if (mColorModePreference != null) {
-            mColorModePreference.stopListening();
-        }
     }
 
     @Override
@@ -672,9 +642,6 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
         updateMobileDataAlwaysOnOptions();
         updateSimulateColorSpace();
         updateUSBAudioOptions();
-        if (mColorTemperaturePreference != null) {
-            updateColorTemperature();
-        }
     }
 
     private void updateAdbOverNetwork() {
@@ -1242,18 +1209,6 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
         }
     }
 
-    private void updateColorTemperature() {
-        updateSwitchPreference(mColorTemperaturePreference,
-                SystemProperties.getBoolean(COLOR_TEMPERATURE_PROPERTY, false));
-    }
-
-    private void writeColorTemperature() {
-        SystemProperties.set(COLOR_TEMPERATURE_PROPERTY,
-                mColorTemperaturePreference.isChecked() ? "1" : "0");
-        pokeSystemProperties();
-        Toast.makeText(getActivity(), R.string.color_temperature_toast, Toast.LENGTH_LONG).show();
-    }
-
     private void updateUSBAudioOptions() {
         updateSwitchPreference(mUSBAudio, Settings.Secure.getInt(getContentResolver(),
                 Settings.Secure.USB_AUDIO_AUTOMATIC_ROUTING_DISABLED, 0) != 0);
@@ -1779,8 +1734,6 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
             writeLegacyDhcpClientOptions();
         } else if (preference == mMobileDataAlwaysOn) {
             writeMobileDataAlwaysOnOptions();
-        } else if (preference == mColorTemperaturePreference) {
-            writeColorTemperature();
         } else if (preference == mUSBAudio) {
             writeUSBAudioOptions();
         } else if (INACTIVE_APPS_KEY.equals(preference.getKey())) {
